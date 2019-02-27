79a900797eab15ae64264d9cfa574fa0122af048
Vincent D <lokajii@yahoo.com>
2019-02-26 17:18:00 -0800

    add page for create cert

--- src/index.js
+++ src/index.js
@@ -10,12 +10,14 @@ import {firebaseApp} from "./firebase";
 import { createBrowserHistory } from 'history';
 import Error from "./Error";
 import Logout from "./logout";
+import CreateCert from "./CreateCert"
+
 const browserHistory = createBrowserHistory()
 firebaseApp.auth().onAuthStateChanged(user => {
     if(user){
         console.log('user signed in or up', user);
-        if(window.location.pathname != "/")
-            browserHistory.push('/');
+         if(window.location.pathname == "/login")
+             browserHistory.push('/');
     } else{
         console.log('user has signed out or still needs to sign in');
         if(window.location.pathname != "/signup")
@@ -29,6 +31,7 @@ ReactDOM.render(
     <Route exact path='/login' component={Login} />
     <Route exact path='/signUp' component={RegisterForm}/>
     <Route exact path='/logout' component={Logout}/>
+    <Route exact path='/create' component={CreateCert} />
     <Route component={Error}/>
     </Switch>
     </Router>, document.getElementById('root')
