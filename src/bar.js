2de6c31eb710c3f78c35d654a9368a45fbec98af
jvan1997 <jonathanvan.1997@gmail.com>
2019-03-15 12:48:08 -0700

    edited the one for the unsigned home page

--- src/bar.js
+++ src/bar.js
@@ -103,7 +103,7 @@ else{
 	<nav class="flex items-center justify-between flex-wrap bg-purple-light p-2">
 	<div class="flex items-center flex-no-shrink text-white mr-6">
 	<img class="border rounded fill-current mr-2 " width="60" height="60" alt="" style={{cursor:'pointer'}} onClick={e => this.goToImg(e)} src={logo} />
-	<button class="font-bold text-xl tracking-tight text-white hover:text-grey-light" value="" style={{cursor:'pointer'}} onClick={e => this.goToImg(e)} >Alethia</button>
+	<button class="font-bold text-xl tracking-tight text-white hover:text-grey-light" value="" style={{cursor:'pointer'}} onClick={e => this.goTo(e)} >Alethia</button>
 		</div>
 <div class="block lg:hidden">
 	<button class="flex items-center px-3 py-2 border rounded text-teal-lighter border-teal-light hover:text-white hover:border-white">
