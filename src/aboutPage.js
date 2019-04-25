b75e4d0b1f880220618a88236517f529ad485391
jvan1997 <jonathanvan.1997@gmail.com>
2019-04-25 00:04:34 -0700

    missed the about page for particles

--- src/aboutPage.js
+++ src/aboutPage.js
@@ -4,6 +4,7 @@ import {firebaseApp} from "./firebase";
 import {RegisterForm} from "./signUp";
 import { Button} from 'react-bootstrap';
 import './App.css';
+import Particles from 'react-particles-js';
 import { withRouter } from 'react-router-dom';
 import Bar from './bar';
 import About from './about';
@@ -15,9 +16,31 @@ class AboutPage extends React.Component {
 
 	render() {
     	return (
-	<div class="bg-cover-image h-full">
+			<div>
+			<Particles
+				className="bg-cover-image fixed w-screen h-screen z-n1"
+				params={{
+					"particles": {
+						"number": {
+							"value": 100
+						},
+						"size": {
+							"value": 3
+						}
+					},
+					"interactivity": {
+						"events": {
+							"onhover": {
+								"enable": true,
+								"mode": "repulse"
+							}
+						}
+					}
+				}}>          </Particles>
+						<div class="z-n2">
 		<Bar />
         <About />
+	</div>
 	</div>
 	)	
 	}
