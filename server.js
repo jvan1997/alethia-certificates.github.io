b685bf5a926f98852cad8c5877c8351939996600
Vincent D <lokajii@yahoo.com>
2019-05-08 18:05:30 -0700

    clean up server.js

--- server.js
+++ server.js
@@ -5,22 +5,18 @@ const pdfjsLib = require('pdfjs-dist')
 let deanSig = "12"
 
 http.createServer(function (req, res) {
-
+  // Handle CORS pre-flight
   if( req.method == "OPTIONS" && req.url == "/check"){
     res.writeHead(200, {"Access-Control-Allow-Origin":"*", "Access-Control-Headers":"accept"})
            
           res.end()
         return
         }
-  
 
-    // console.log(req)
-    console.log(req.url)
-    console.log(req.method)
+    // Process submitted data
     var busboy = new Busboy({ headers: req.headers })
     let body = {}
     busboy.on('field', function(fieldname, val, fieldnameTruncated, valTruncated, encoding, mimetype) {
-      console.log(fieldname)
       body[fieldname] = val
     })
 
@@ -45,23 +41,22 @@ http.createServer(function (req, res) {
       console.log('Done parsing form!');
       console.log(body)
 
+      // Verify that submitted deanSig is correct
       if( req.method == "POST" && req.url == "/check"){
         if( body.sigid === deanSig){
 
           if( typeof(body.file) == "undefined"){
             res.writeHead(200, {'Content-Type': 'text', "Access-Control-Allow-Origin":"*"})
-              res.write('Valid deanSig'); //write a response
+              res.write('Valid deanSig'); 
               res.end()
             return
           }
 
-
+          // Process pdf file
           getPDFText(body.file)
             .then((text) => {
-              // console.log(`text is ${text}`)
 
               let strs = text.split("\n")
-              // console.log(strs)
               let name = ""
               let major = ""
               let units = ""
@@ -94,18 +89,15 @@ http.createServer(function (req, res) {
 
                 if (strs[i].includes(nameToken)) {
                   name = strs[i].split(nameToken)[1].trim()
-                  // console.log(name)
                   returnObject.name = name
                 }
                 if (strs[i].includes(majorToken)) {
                   major = strs[i].split(majorToken)[1].trim()
-                  // console.log(major)
                   returnObject.major = major
                 }
                 if (strs[i].includes(schoolUnits)) {
                   units = strs[i].split(schoolUnits)[1].trim()
                   let totalUnits = units.split(/\s+/)[3]
-                  // console.log(totalUnits)
                   returnObject.units = totalUnits
                 }
 
@@ -113,12 +105,11 @@ http.createServer(function (req, res) {
               return returnObject
             })
             .then(result=>{
-              // console.log(`result is ${result}`)
               for (const key in result){
                 console.log(result[key])
               }
               res.writeHead(200, {'Content-Type': 'text', "Access-Control-Allow-Origin":"*"})
-              res.write('Valid deanSig'); //write a response
+              res.write('Valid deanSig'); 
               res.end()
             })
 
@@ -129,15 +120,14 @@ http.createServer(function (req, res) {
         }
         else{
           res.writeHead(422, {'Content-Type': 'text', "Access-Control-Allow-Origin":"*"})
-          res.write('Invalid deanSig'); //write a response
+          res.write('Invalid deanSig');
           res.end()
         }
         return
         
       }
       else{
-        res.write('Hello World!'); //write a response to the client
-        res.end(); //end the response
+        res.end(); 
       }
 
 
@@ -151,30 +141,23 @@ http.createServer(function (req, res) {
   }).listen(8080); //the server object listens on port 8080
 
   /**
-     * 
+     * Read submitted pdf file
      * @param {*} data Is pdf read as data url
      */
     async function getPDFText(data){
       var loadingTask = pdfjsLib.getDocument(data);
       let pdf = await loadingTask.promise
-      // console.log(pdf)
 
       let pdfText = ''
 
-      // console.log(pdf.numPages)
       for(var i=1;i<=pdf.numPages;i++){
           let page = await pdf.getPage(i)
-          // console.log(page)
           let textContent = await page.getTextContent()
-          // console.log(textContent)
 
           pdfText = textContent.items.reduce( (acc,curr) => acc + curr.str +"\n", pdfText )
-          // for( var j=0; j<textContent.items.length;j++){
-          //     pdfText += textContent.items[j].str + "\n"
-          // }
+
       }
 
-      // console.log(pdfText)
       
       return pdfText
   }
